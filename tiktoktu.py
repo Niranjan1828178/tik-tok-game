fd406da15ae56fd049e128a82fe8c1cc3d38327b
niranjan <niranjan64461231@gmail.com>
2024-05-17 16:21:07 +0530

    sdfsdf

--- tiktoktu.py
+++ tiktoktu.py
@@ -17,7 +17,7 @@ def check(playercount, computercount):
                 
                 if playercount>=3:
                     print("Player wins")
-                    return True
+                    exit()
             else:
                 playercount=0
         if playercount>=3:
@@ -29,7 +29,7 @@ def check(playercount, computercount):
                 computercount+=1
                 if computercount>=3:
                     print("computer wins")
-                    return True
+                    exit()
             else:
                 computercount=0
         if computercount>=3:
@@ -44,7 +44,7 @@ def Print_matrix():
 
 Print_matrix()
 
-while not(Winstate):
+while True:
     if turn=='player':
         position=int(input("Player's turn\nEnter the position to draw X: "))
         if position<=9 and slate[position]=='   ':
@@ -55,54 +55,64 @@ while not(Winstate):
         else:
             if position>9 or slate[position]!='   ':
                 print("Invalid Position, Try again!")
+        boardfull=[]
+        for q in range(0,9):
+            if slate[q+1] != '   ':
+                boardfull.append(q)
+            if len(boardfull)>=9:
+                print("Game Over")
+                exit()
                 
     if turn=='computer':
         print("Computer's turn now")
         turnover=False
         countx=0
         for i in [1,2,3,4,5,6,7,8,9]:
-            if slate[i]==' X ' and turnover==False:
-                countx+=1
-                poss={1:[[2,3],[4,7],[5,9]],2:[[1,3],[5,8]],3:[[1,2],[5,7],[6,9]],
-                      4:[[5,6],[1,7]],5:[[1,9],[4,6],[2,8],[3,7]],6:[[3,9],[4,5]],7:[[1,4],[5,3],[8,9]],8:[[2,4],[7,9]],9:[[1,5],[7,8],[3,6]]}
-                temp=poss[i]
-                for j in temp:
-                    sub=j
-                    for k in j:
-                        if slate[sub[0]]==' X ' or slate[sub[1]]==' X ':
-                            if slate[sub[0]]==' X ':
-                                    slate[sub[1]]=' O '
-                                    turn='player'
-                                    turnover=True
-                            if slate[sub[1]]==' X ':
-                                    slate[sub[0]]=' O '
-                                    turn='player'
-                                    turnover=True
-            if slate[i]==' O ' and turnover==False:
+            if slate[i]==' O ' and not turnover:
                 counto+=1
                 poss={1:[[2,3],[4,7],[5,9]],2:[[1,3],[5,8]],3:[[1,2],[5,7],[6,9]],
-                      4:[[5,6],[1,7]],5:[[1,9],[4,6],[2,8],[3,7]],6:[[3,9],[4,5]],7:[[1,4],[5,3],[8,9]],8:[[2,4],[7,9]],9:[[1,5],[7,8],[3,6]]}
+                      4:[[5,6],[1,7]],5:[[1,9],[4,6],[2,8],[3,7]],6:[[3,9],[4,5]],7:[[1,4],[5,3],[8,9]],8:[[2,5],[7,9]],9:[[1,5],[7,8],[3,6]]}
                 temp=poss[i]
                 for j in temp:
                     sub=j
-                    for k in j:
-                        if slate[sub[0]]==' O ' or slate[sub[1]]==' O ':
-                            if slate[sub[0]]==' O ':
-                                    slate[sub[1]]=' O '
-                                    turn='player'
-                                    turnover=True
-                            if slate[sub[1]]==' O ':
-                                    slate[sub[0]]=' O '
-                                    turn='player'
-                                    turnover=True
-                                
-        if countx<2 and turnover==False:
+                    if slate[sub[0]]==' O ' or slate[sub[1]]==' O ':
+                        if slate[sub[0]]==' O ' and slate[sub[1]]=='   ':
+                                slate[sub[1]]=' O '
+                                turn='player'
+                                turnover=True
+                                countx=0
+                        if slate[sub[1]]==' X ' and slate[sub[0]]=='   ':
+                                slate[sub[0]]=' O '
+                                turn='player'
+                                turnover=True
+                                countx=0
+        for i in [1,2,3,4,5,6,7,8,9]:
+            if slate[i]==' X ' and not countx>=3 and not turnover:
+                countx=1
+                poss={1:[[2,3],[4,7],[5,9]],2:[[1,3],[5,8]],3:[[1,2],[5,7],[6,9]],
+                      4:[[5,6],[1,7]],5:[[1,9],[4,6],[2,8],[3,7]],6:[[3,9],[4,5]],7:[[1,4],[5,3],[8,9]],8:[[2,5],[7,9]],9:[[1,5],[7,8],[3,6]]}
+                temp=poss[i]
+                for j in temp:
+                    sub=j
+                    if slate[sub[0]]==' X ' or slate[sub[1]]==' X ':
+                        if slate[i]==' X ' and slate[sub[0]]==' X ' and slate[sub[1]]=='   ':
+                                slate[sub[1]]=' O '
+                                turn='player'
+                                turnover=True
+                                countx+=1
+                        if slate[i]==' X ' and slate[sub[1]]==' X ' and slate[sub[0]]=='   ':
+                                slate[sub[0]]=' O '
+                                turn='player'
+                                turnover=True
+                                countx+=1
+           
+        if countx<2 and not turnover:
+            
             inserted=False
             while not(inserted):
                 pos=random.randint(1,9)
                 if slate[pos]==' X ' or slate[pos]==' O ':
                     pos=random.randint(1,9)
-
                 else:
                     slate[pos] = ' O '
                     inserted=True
@@ -112,10 +122,15 @@ while not(Winstate):
         Print_matrix()
         Winstate = check(playercount, computercount)
         turn='player'
-    boardfull=[]
-    for q in range(0,9):
-        if slate[q+1] != '   ':
-            boardfull.append(q)
-        if len(boardfull)>=9:
-            print("Game Over")
-            exit
+        boardfull=[]
+        for q in range(0,9):
+            if slate[q+1] != '   ':
+                boardfull.append(q)
+            if len(boardfull)>=9:
+                print("Game Over")
+                exit()
+        
+    
+
+
+
